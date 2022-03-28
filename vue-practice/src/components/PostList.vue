1cd58536c41bf5f7b0f4a9886e9904be74ad7e3f
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-03-28 17:01:18 +0300

    Create post-list component

---
+++ vue-practice/src/components/PostList.vue
@@ -0,0 +1,39 @@
+<template>
+
+	<div>
+		<post-form :posts="posts"></post-form>
+		<div class="post" v-for="post in posts">
+			<post-row :post="post"></post-row>
+		</div>
+	</div>
+
+</template>
+
+<script>
+	import PostForm from './PostForm.vue'
+	import PostRow from './PostRow.vue'
+	
+	export default {
+		data() {
+			return {
+				posts: [
+					{id: 0, title: 'JavaScript post', description: 'universal programming language'},
+					{id: 1, title: 'Java post', description: 'universal programming language'},
+					{id: 2, title: 'C++ post', description: 'universal programming language'},
+				],
+			}
+		},
+		components: {
+			PostForm,
+			PostRow
+		}
+	}
+</script>
+
+<style>
+	.post {
+		padding: 15px;
+		border: 3px solid teal;
+		margin-top: 15px;
+	}
+</style>
