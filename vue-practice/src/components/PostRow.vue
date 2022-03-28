93d4c06a0406e33b7caa15b4e24af8051bd5e168
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-03-28 17:00:57 +0300

    Create post-row component

---
+++ vue-practice/src/components/PostRow.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<div>
+			<strong>Name: </strong> {{post.title}}
+		</div>
+		<div>
+			<strong>Text: </strong> {{post.description}}
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['post']
+	}
+</script>
+
+<style>
+
+</style>
