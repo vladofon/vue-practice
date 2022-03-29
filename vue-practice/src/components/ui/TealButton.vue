f7ee179bb6fe3d6f875fafbf5b7b9c585117b161
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-03-29 13:46:08 +0300

    Create teal-button component

---
+++ vue-practice/src/components/ui/TealButton.vue
@@ -0,0 +1,19 @@
+<template>
+	<button class="btn">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+	export default {
+		name: 'teal-button'
+	}
+</script>
+
+<style>
+	.btn {
+		padding: 10px 15px;
+		background: none;
+		border: 1px solid teal;
+	}
+</style>
