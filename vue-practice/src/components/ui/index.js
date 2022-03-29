c533d0b6111539bdb48157094d4939576a88ddf2
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-03-29 13:47:49 +0300

    Make ui components global

---
+++ vue-practice/src/components/ui/index.js
@@ -0,0 +1,7 @@
+import TealButton from '@/components/ui/TealButton.vue'
+import TealInput from '@/components/ui/TealInput.vue'
+
+export default [
+	TealButton,
+	TealInput
+]
--- vue-practice/src/main.js
+++ vue-practice/src/main.js
@@ -1,4 +1,11 @@
+import components from '@/components/ui/index'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+components.forEach(component => {
+	app.component(component.name, component)
+})
+
+app.mount('#app')
