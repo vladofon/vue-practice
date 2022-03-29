fa590cfdef71ba04cb968973448c5b9e2a57622b
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-03-29 13:46:45 +0300

    Create teal-input component

---
+++ vue-practice/src/components/ui/TealInput.vue
@@ -0,0 +1,17 @@
+<template>
+	<input class="input" type="text">
+</template>
+
+<script>
+	export default {
+		name: 'teal-input'
+	}
+</script>
+
+<style>
+	.input {
+		width: 100%;
+		border: 1px solid teal;
+		padding: 10px 15px;
+	}
+</style>
