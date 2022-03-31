c1e2f692854fbcf166912debe037134ea0455d2f
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-03-31 18:38:33 +0300

    Create Main and About pages

---
+++ vue-practice/src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+	<h2>About</h2>
+	<br>
+	<p>This Vue.js based website was created for educational purposes.</p>
+	<p>More sites for God Of Sites!</p>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- vue-practice/src/router/router.js
+++ vue-practice/src/router/router.js
@@ -1,10 +1,12 @@
 import Main from '@/pages/Main.vue'
 import Posts from '@/pages/Posts.vue'
+import About from '@/pages/About.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
 	{path: '/', component: Main},
-	{path: '/posts', component: Posts}
+	{path: '/posts', component: Posts},
+	{path: '/about', component: About}
 ]
 
 const router = createRouter({
