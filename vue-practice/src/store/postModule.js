08855180bd08d437110c20e7b1220e2cb4b68487
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-02 14:50:51 +0300

    Create post-store module

---
+++ vue-practice/src/store/postModule.js
@@ -0,0 +1,20 @@
+export default {
+	state: () => ({
+		posts: [],
+		isPostsLoading: false,
+		page: 1,
+		limit: 10,
+		totalPages: 0,
+		searchQuery: '',
+		selectedSort: '',
+	}),
+	getters: {
+		
+	},
+	mutations: {
+		
+	},
+	actions: {
+		
+	}
+}
